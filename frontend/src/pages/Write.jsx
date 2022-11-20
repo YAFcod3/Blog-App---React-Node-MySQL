cc494a12a790a4e574925873ad8c0a7371dadfe4
yasmani <yasmaniarmas1995@gmail.com>
2022-11-19 23:46:08 -0500

    front, write ok

--- frontend/src/pages/Write.jsx
+++ frontend/src/pages/Write.jsx
@@ -1,9 +1,87 @@
+import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 const Write = () => {
+  const [value, setValue] = useState("");
+  //console.log(value)
   return (
     <>
-      <div>Write</div>
+      <div className="add">
+        {/*Content */}
+        <div className="content">
+          <input type="text" placeholder="title" />
+          <div className="editorContainer">
+            <ReactQuill
+              className="editor"
+              theme="snow"
+              value={value}
+              onChange={setValue}
+            />
+          </div>
+        </div>
+        {/**Menu */}
+        <div className="menu">
+          <div className="item">
+            <h1>Publish</h1>
+            <span>
+              <b>Status:</b>Draft
+            </span>
+            <span>
+              <b>Visibility:</b>Public
+            </span>
+            <label className="file" htmlFor="file">
+              Upload Image
+            </label>
+            <input style={{ display: "none" }} type="file" id="file" />
+            <div className="buttons">
+              <button>Save as a draft</button>
+              <button>Update</button>
+            </div>
+          </div>
+          <div className="item">
+            <h1>Category</h1>
+            <div className="cat">
+              <input type="radio" name="cat" value="art" id="art" />
+
+              <label htmlFor="art">Art</label>
+            </div>
+
+            <div className="cat">
+              <input type="radio" name="cat" value="science" id="science" />
+              <label htmlFor="art">Science</label>
+            </div>
+
+            <div className="cat">
+              {" "}
+              <input
+                type="radio"
+                name="cat"
+                value="technology"
+                id="technology"
+              />
+              <label htmlFor="art">Technology</label>
+            </div>
+            <div className="cat">
+              {" "}
+              <input type="radio" name="cat" value="cinema" id="cinema" />
+              <label htmlFor="art">Cinema</label>
+            </div>
+            <div className="cat">
+              {" "}
+              <input type="radio" name="cat" value="design" id="design" />
+              <label htmlFor="art">Design</label>
+            </div>
+            <div className="cat">
+              {" "}
+              <input type="radio" name="cat" value="food" id="food" />
+              <label htmlFor="art">Food </label>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
-export default Write
+export default Write;
